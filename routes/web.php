63f4c74c8208e3015d41fa4beb6cca8a18c27141
remonhasan <remonhasan041@gmail.com>
2021-01-09 21:06:29 +0600

    added newly routes with author/admin groups

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,16 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth','admin']], function (){
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+});
+
+Route::group(['as'=>'author.','prefix'=>'author','namespace'=>'Author','middleware'=>['auth','author']], function (){
+    Route::get('dashboard','DashboardController@index')->name('dashboard');
+});
